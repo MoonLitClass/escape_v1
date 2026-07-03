40231700f94cfdab0790530cf11d00b276660725
Moon_Lit <hyeonwoo1023@gmail.com>
2026-07-03 14:46:05 +0900

    V1 시연

---
+++ explorer/v1/js/effects.js
@@ -0,0 +1,91 @@
+/* ======================================================
+   Escape Engine v1.0
+   효과
+====================================================== */
+
+
+/* ======================================================
+    화면 흔들기 (오답)
+====================================================== */
+
+function shakeScreen(){
+
+    const container =
+    document.querySelector(".game-container");
+
+    container.classList.add("shake");
+
+    setTimeout(()=>{
+
+        container.classList.remove("shake");
+
+    },500);
+
+}
+
+
+/* ======================================================
+    이미지 페이드
+====================================================== */
+
+function fadeImage(src){
+
+    gameImage.style.opacity = "0";
+
+    const img = new Image();
+
+    img.onload = function(){
+
+        gameImage.src = src;
+
+        gameImage.style.opacity = "1";
+
+    };
+
+    img.src = src;
+
+}
+
+/* ======================================================
+    정답 효과
+====================================================== */
+
+function successEffect(){
+
+    gameImage.classList.add("success");
+
+    setTimeout(()=>{
+
+        gameImage.classList.remove("success");
+
+    },500);
+
+}
+
+
+/* ======================================================
+    오답 효과
+====================================================== */
+
+function wrongEffect(){
+
+    shakeScreen();
+
+}
+
+
+/* ======================================================
+    버튼 클릭 효과
+====================================================== */
+
+function buttonEffect(button){
+
+    button.classList.add("button-click");
+
+    setTimeout(()=>{
+
+        button.classList.remove("button-click");
+
+    },150);
+
+}
